0af02a89b1ab3fa32efa978b3a925932e145f70d
Danila Gusakov <ivanscourgeblackstar@gmail.com>
2024-05-26 03:03:51 +0300

    Zalupa

--- ansible/entrypoint.sh
+++ ansible/entrypoint.sh
@@ -3,11 +3,14 @@ ulimit -n 65536
 
 echo "hacluster:123" | chpasswd
 
-service ssh start
-service nginx start
-service pacemaker start
-service corosync start
-service pcsd start
+# rm -rf /etc/corosync/corosync.conf
+pcs cluster destroy --force
+# cp corosync.conf /etc/corosync/corosync.conf
+# supervisorctl reread && supervisorctl update all && supervisorctl restart
+
+/usr/local/bin/supervisord -n -c /etc/supervisor/supervisord.conf &
+
+ansible-playbook -i ansible/inventory/inventory ansible/playbooks/playbook_main.yml
 
 # Запустить бесконечный цикл, чтобы контейнер не завершил выполнение
 tail -f /dev/null
